8bd4d8fe0886dbc171e4e085906cd2b61e8276a8
karthick <karthickthekaiser@gmail.com>
2012-06-07 21:31:06 +0530

    commit2

--- config/routes.rb
+++ config/routes.rb
@@ -134,7 +134,7 @@ devise_for :users, :controllers => {:registrations => "registrations"}
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => "welcome#index"
-
+     
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
