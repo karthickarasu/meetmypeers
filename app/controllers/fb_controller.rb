276dd95fc16e116d0d7a250b31c9f73788d7cade
karthickarasu <karthickthekaiser@gmail.com>
2012-06-18 14:16:06 +0530

    12commit

--- app/controllers/fb_controller.rb
+++ app/controllers/fb_controller.rb
@@ -11,23 +11,13 @@ def create
 	
 	@user=User.find_by_id(current_user.id)
     @user.update_attributes(params[:user])
-           # @user = User.find_by_id(current_user.id)
-       #@user.update_attributes(params[:user])
-puts params[:user].inspect
-
-            redirect_to '/home/profilepage'
+         redirect_to '/home/profilepage'
 
 
 end
 def viewtime
  @user=User.find_all_by_id(current_user.id)
-p 11111111111111
-p @user
-    respond_to do |format|
-    format.html # show.html.erb
-   format.xml  { render :xml => @user }
-      #@user=User.find_all_by_user_id(current_user.id)
-end
+
 
     end
     
